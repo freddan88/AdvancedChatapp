989e5e365a63c09fe4594e086c793039d98a2a8f
freddan88 <fredrik@leemann.se>
2021-01-20 17:30:27 +0100

    New try to deploy on heroku

---
+++ client/build/precache-manifest.60859d94be5bcaf03f4532633d21915d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc28a74e72c906c6ba1719e9f02be56d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "39183a7de0f741dffc79",
+    "url": "/static/css/main.b23c8e22.chunk.css"
+  },
+  {
+    "revision": "8c35f9056da900316802",
+    "url": "/static/js/2.407dd848.chunk.js"
+  },
+  {
+    "revision": "188fb33365c8afefca5570e89b24f637",
+    "url": "/static/js/2.407dd848.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39183a7de0f741dffc79",
+    "url": "/static/js/main.e8e5e175.chunk.js"
+  },
+  {
+    "revision": "08a01cac7a9424eef63f",
+    "url": "/static/js/runtime-main.ef76e641.js"
+  }
+]);
